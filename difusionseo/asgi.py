b453ce97bed5dbb4a44c87bb3a71fdfcbc5ab2b4
AbRehmanSaif <abdulrehmanarain713@gmail.com>
2026-04-15 01:35:39 +0500

    first commit

---
+++ difusionseo/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for difusionseo project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'difusionseo.settings')
+
+application = get_asgi_application()
