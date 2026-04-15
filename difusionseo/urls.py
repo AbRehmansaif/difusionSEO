d2e7ad70fe507a842c551bcb3b2406bfa2b725c2
AbRehmanSaif <abdulrehmanarain713@gmail.com>
2026-04-15 20:04:52 +0500

    robots, sitemap

--- difusionseo/urls.py
+++ difusionseo/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    path('sitemap.xml', TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml")),
     path('', include('core.urls')),
 ]
